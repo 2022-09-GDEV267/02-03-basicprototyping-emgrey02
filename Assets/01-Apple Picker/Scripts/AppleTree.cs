da2b828a42615a0f9cd46b5632bd721eabd23a6d
Emma Grey <emgrey02@gmail.com>
2022-09-08 12:50:50 -0400

    Make tree move and apples drop

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    // chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    // rate at which apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // dropping apples every second
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //changing direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+        
+    }
+
+    // time based
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; // change direction
+        }
+    }
+}
